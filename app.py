60dd926975cdedc15eeadde12dbad1ccce2ddfdc
dongduong <dtrinh1@lakeheadu.ca>
2020-10-27 21:25:23 +0700

    update the app.py and add some html templates.

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# New code
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("../COMP4312-AnimalRecognition/templates/index.html")
 
 
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-#
-#
-# if __name__ == '__main__':
-#     app.run()
+@app.route('/about')
+def about():
+    return render_template("../COMP4312-AnimalRecognition/templates/about.html")
+
+
+@app.route('/inference', methods=('GET','POST'))
+def inference():
+    return render_template("../COMP4312-AnimalRecognition/templates/inference.html")
+
+
+if __name__ == '__main__':
+    app.run()
